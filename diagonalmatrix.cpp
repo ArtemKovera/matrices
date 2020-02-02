035f89f55f956f7b58fff8a4959636b6129b87fd
Artem Kovera <artemkovera@gmail.com>
2020-02-02 20:48:21 +0200

    class for square diagonal matrix

---
+++ diagonalmatrix.cpp
@@ -0,0 +1,92 @@
+//C++ class for square diagonal matrix (N x N) using stl vector
+
+#include<iostream>
+#include<vector>
+#include <cassert> 
+#include <stdexcept>
+
+
+class SquareDiagonalMatrix
+{
+
+private:
+
+    //square diagonal matrix is represented by one-dimensional vector
+    std::vector<int> matrix;
+
+public:
+
+    
+    SquareDiagonalMatrix (int rows, int columns)
+    {
+       //number of rows must be equal to the number of columns
+       assert(rows==columns);
+       // and both must be positive
+       assert(rows>0 and columns>0);
+
+       matrix.reserve(rows); 
+    }
+    
+    
+    int getElement (int row, int column)
+    {   
+        if (row < 1 or column < 1 or row > matrix.capacity() or column > matrix.capacity())
+        {
+            throw std::out_of_range("Please enter appropriate coordinates for the element");
+        }
+        else if (row==column)
+        {
+            return matrix[row - 1];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    int getElement (int row)
+    {
+        if (row < 1 or row > matrix.capacity())
+        {
+            throw std::out_of_range("Please enter appropriate row");
+        }
+
+        return matrix[row - 1];
+    }
+    
+
+    void setElement (int row, int value)
+    {
+        if (row < 1 or row > matrix.capacity())
+        {
+            throw std::out_of_range("Please enter appropriate row");
+        }
+
+        matrix[row-1] = value; 
+    } 
+
+};
+
+int main ()
+{
+    
+    
+    SquareDiagonalMatrix dm1(10, 10);
+    
+    
+    
+    try
+    {
+    dm1.setElement(1, 25);
+    std::cout << dm1.getElement(1) << std::endl;
+    std::cout << dm1.getElement(9) << std::endl;
+    std::cout << dm1.getElement(12, 12) << std::endl;
+    }
+    catch(std::out_of_range& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    return 0;
+}
