f2438c098b03deecee7b156c635b9fa14dfd9b48
Artem Kovera <artemkovera@gmail.com>
2020-05-08 14:39:36 +0300

    added showMatrix method

--- sparsematrix.cpp
+++ sparsematrix.cpp
@@ -17,6 +17,10 @@ public:
 
     int getNumberOfNotZeros() const;
 
+    //this function prints the matrix in the console
+    //the time complexity of this function is O(n^3)
+    void showMatrix() const;
+
 private:
     struct Element;
     int rows;
@@ -31,10 +35,14 @@ private:
 
 int main ()
 {
-    SparseMatrix sm1(10, 10, 7);
+    SparseMatrix sm1(10, 10, 3);
 
     std::cout << "There are " << sm1.getNumberOfNotZeros() << " not zero matrix elements" << std::endl;
+    
 
+    std::cout<<std::endl;
+
+    sm1.showMatrix();
 
     return 0;
 }
@@ -49,25 +57,26 @@ struct SparseMatrix::Element
 SparseMatrix::SparseMatrix(int rows, int columns, int elementNumber)
 {
     if(elementNumber > (rows * columns))
-        throw std::invalid_argument("The number of elements (third argument) must be less than the product of rows * columns");
+        throw std::invalid_argument("The number of elements (third argument) must not be greater than the product of rows * columns");
 
     this->rows = rows;
     this->columns = columns;
     numberOfNotZeros = elementNumber;
     arrayPointer = new Element [numberOfNotZeros];
-
+    int i = 1; 
     if(numberOfNotZeros > 0)
     {
         std::cout << "Please enter the " << numberOfNotZeros << " element" << (numberOfNotZeros > 1 ? "s" : "") << std::endl;
         std::cout << "The values of the row number, column number and the element's value must be separated by an empty space" << std::endl;
         
-        for(int i = 1; i<=numberOfNotZeros; i++)
+        while( i != (numberOfNotZeros + 1))
         {
             std::cout << i << "s" << " element: ";
             if(std::cin.good())
                std::cin >> arrayPointer[i].rowNumber >> arrayPointer[i].columnNumber >> arrayPointer[i].value;
             else
                 std::cout << "Sorry, for some reason the input isn't working properly" << std::endl;
+            i++;
         } 
 
         std::cout << "All the elements have just been included to the matrix" << std::endl;  
@@ -79,4 +88,31 @@ int SparseMatrix::getNumberOfNotZeros() const
     return numberOfNotZeros;
 }
 
+void SparseMatrix::showMatrix() const
+{
+    int i, j, k;
+    bool flag = true;
+    for(i=0; i<rows; i++)
+    {
+        for(j=0; j<columns; j++)
+        {
+            flag = true;
+            k = 1;
+            while( k != (numberOfNotZeros + 1))
+            {
+                if(arrayPointer[k].rowNumber == i && arrayPointer[k].columnNumber == j)
+                {
+                    std::cout << arrayPointer[k].value << " ";
+                    flag = false;
+                    break;
+                }
+                k++;
+            }
+            if(flag) 
+                std::cout << "0 ";
+            
+        }
+        std::cout << std::endl;
+    }
+}
 
